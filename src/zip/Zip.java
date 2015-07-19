67fd70469845fe34f189d34c1eca07b8ca2d8e16
murvit <murvit@ukr.net>
2015-07-20 00:38:52 +0300

    from notebook

--- src/zip/Zip.java
+++ src/zip/Zip.java
@@ -2,14 +2,14 @@ package zip;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Arrays;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * worked on it
@@ -22,8 +22,17 @@ public class Zip extends HttpServlet {
         byte[] data = new byte[1024 * 1024 * 5];
         int length = bis.read(data);
         byte[] inputData = Arrays.copyOf(data, length);
-        String out = new String (inputData);
-        resp.getWriter().write(out);
+        String out = new String(inputData);
+
+  //      resp.getWriter().write(out);
+ //       resp.setContentType("application");
+
+        ZipOutputStream zout = new ZipOutputStream(resp.getOutputStream());
+        zout.putNextEntry(new ZipEntry("111.txt"));
+        zout.write(inputData);
+        zout.closeEntry();
+        zout.close();
+
 //        String html = "<html><head><title>Test</title></head><body> %s </body></html>";
 //        String getback = "<p><a href=\"anketa.html\">Get back to Questionnaire project</a></p>";
 //        String getHome = "<p><a href=\"index.html\">Go Home</a></p>";
