e0d77fa5dae28c716d5d0f93726b6d1b20c0743a
Vit <murvit@ukr.net>
2015-07-21 16:48:45 +0300

    lttl impr

--- src/zip/Zipper.java
+++ src/zip/Zipper.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.util.Arrays;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -23,12 +22,10 @@ import java.util.zip.ZipOutputStream;
 public class Zipper extends HttpServlet {
 
     String filename;
-    byte[] cleanFile;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-//      uploading file
         ServletFileUpload upload = new ServletFileUpload();
         try {
             FileItemIterator iterator = upload.getItemIterator(req);
@@ -39,44 +36,30 @@ public class Zipper extends HttpServlet {
                     item.getFieldName();
                 else
                     filename = item.getName();
-                byte[] allData = new byte[1024 * 1024 * 5];
-                int i = 0;
-                int counter = 0;
-                while ((i = inputStream.read(allData, counter, 1024)) > 0) {
-                    counter += i;
+
+                int lastSlash = filename.lastIndexOf("\\");
+                if (lastSlash > 0)
+                    filename = filename.substring(lastSlash);
+
+                resp.setContentType("application/zip");
+                resp.setHeader("Content-Disposition", "inline; filename=" + filename + ".zip;");
+
+
+                ServletOutputStream servletOutputStream = resp.getOutputStream();
+                ZipOutputStream zipOutputStream = new ZipOutputStream((servletOutputStream));
+                zipOutputStream.putNextEntry(new ZipEntry(filename));
+
+                int len;
+                byte[] buffer = new byte[8192];
+                while ((len = inputStream.read(buffer, 0, buffer.length)) != -1) {
+                    zipOutputStream.write(buffer, 0, len);
                 }
-                cleanFile = Arrays.copyOf(allData, counter);
-                allData = null;
-                inputStream.close();
+                zipOutputStream.close();
+                servletOutputStream.flush();
+                servletOutputStream.close();
             }
-
-//      search for / in filename
-            int lastSlash = filename.lastIndexOf("\\");
-            if (lastSlash>0){
-                filename = filename.substring(lastSlash);
-            }
-
         } catch (FileUploadException e) {
             e.printStackTrace();
         }
-
-
-//      return zip file
-
-        resp.setContentType("application/zip");
-        resp.setHeader("Content-Disposition", "inline; filename=" + filename + ".zip;");
-        byte[] output = new byte[1024];
-        int len;
-        ServletOutputStream servletOutputStream = resp.getOutputStream();
-        ZipOutputStream zipOutputStream = new ZipOutputStream((servletOutputStream));
-        zipOutputStream.putNextEntry(new ZipEntry(filename));
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(cleanFile);
-        while ((len = byteArrayInputStream.read(output)) != -1) {
-            zipOutputStream.write(output, 0, len);
-        }
-        byteArrayInputStream.close();
-        zipOutputStream.close();
-        servletOutputStream.flush();
-        servletOutputStream.close();
     }
 }
